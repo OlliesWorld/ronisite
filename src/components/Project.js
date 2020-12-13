974edc5b305692b60872b055ea0d8bf39a99edeb
Rtlockwood-O <rtlockwoodwork@gmail.com>
2020-12-12 17:31:58 -0700

    clean and fixed tempalte

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,7 @@ import Unplug from '../assets/images/unplugged1.jpg';
 import Slicks from '../assets/images/slickspizza.png';
 import Form from '../assets/images/active-states.jpg';
 import OllieSite from '../assets/images/olliesworld-site.png';
-import Portfolio from '../assets/images/portfolio-site.png';
+import Jane from '../assets/images/jane.png';
 
 // const boxes =document.querySelectorAll('.example')
 
@@ -57,7 +57,7 @@ const ProjectPage = () => (
           <hr />
           <div className="example">
             <h4>Template Site</h4>
-            <p><span className="image right"><img src={Portfolio} alt="Slicks screenshot" /></span>This is a completely new site for client. There are 2 types of image galleries, testimonial slider, and a contact form. This is built on GatsbyJS Framework for reliability, speed and security. Maecenas et lorem quis sem varius sagittis et a est. Maecenas iaculis iaculis sem. Donec vel dolor at arcu tincidunt bibendum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce ut aliquet justo. Donec id neque ipsum. Integer eget ultricies odio. Nam vel ex a orci fringilla tincidunt. Aliquam eleifend ligula non velit accumsan cursus. Donec id neque ipsum. Integer eget ultricies odio. Nam vel ex a orci fringilla tincidunt. Aliquam eleifend ligula non velit accumsan cursus. Morbi mattis mi consectetur tortor elementum, varius pellentesque velit convallis. Aenean tincidunt lectus auctor mauris maximus, ac scelerisque ipsum tempor.</p>
+            <p><span className="image right"><img src={Jane} alt="Slicks screenshot" /></span>This is a completely new site for client. There are 2 types of image galleries, testimonial slider, and a contact form. This is built on GatsbyJS Framework for reliability, speed and security. Maecenas et lorem quis sem varius sagittis et a est. Maecenas iaculis iaculis sem. Donec vel dolor at arcu tincidunt bibendum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce ut aliquet justo. Donec id neque ipsum. Integer eget ultricies odio. Nam vel ex a orci fringilla tincidunt. Aliquam eleifend ligula non velit accumsan cursus. Donec id neque ipsum. Integer eget ultricies odio. Nam vel ex a orci fringilla tincidunt. Aliquam eleifend ligula non velit accumsan cursus. Morbi mattis mi consectetur tortor elementum, varius pellentesque velit convallis. Aenean tincidunt lectus auctor mauris maximus, ac scelerisque ipsum tempor.</p>
             <h5>GatsbyJS with Image Sliders, Image Grid, Contact Form and custom color scheme</h5>
           </div>
           <hr />
